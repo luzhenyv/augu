e9f2b436a57ecadb53eace7fe01230795a092b49
luzhenyv <luzhencumt@163.com>
2023-06-25 11:24:07 +0800

    add icons

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,48 @@
 <template>
   <v-app :theme="customizeTheme.darkTheme ? 'dark' : 'light'">
-    <!-- <component :is="currentLayout" v-if="isRouterLoaded">
+    <component :is="currentLayout" v-if="isRouterLoaded">
       <router-view> </router-view>
-    </component> -->
+    </component>
     <CustomizationMenu />
     <BackToTop />
   </v-app>
 </template>
 
 <script setup lang="ts">
-// import { computed } from "vue";
-// import { useRoute } from "vue-router";
-// import DefaultLayout from "@/layouts/DefaultLayout.vue";
-// import LandingLayout from "@/layouts/LandingLayout.vue";
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+import DefaultLayout from "@/layouts/DefaultLayout.vue";
+import LandingLayout from "@/layouts/LandingLayout.vue";
 import CustomizationMenu from "@/components/CustomizationMenu.vue";
 import BackToTop from "@/components/common/BackToTop.vue";
 import { useCustomizeThemeStore } from "@/stores/customizeTheme";
 
 const customizeTheme = useCustomizeThemeStore();
 
-// const route = useRoute();
+const route = useRoute();
 
-// const isRouterLoaded = computed(() => {
-//   if (route.name !== null) return true;
-//   return false;
-// });
+const isRouterLoaded = computed(() => {
+  if (route.name !== null) return true;
+  return false;
+});
 
-// const layouts = {
-//   default: DefaultLayout,
-//   // ui: UILayout,
-//   landing: LandingLayout,
-//   // auth: AuthLayout,
-// };
+const layouts = {
+  default: DefaultLayout,
+  // ui: UILayout,
+  landing: LandingLayout,
+  // auth: AuthLayout,
+};
 
 // type LayoutName = "default" | "ui" | "landing" | "auth" | "error";
-// type LayoutName = "default" | "landing";
+type LayoutName = "default" | "landing";
 
-// const currentLayout = computed(() => {
-//   const layoutName = route.meta.layout as LayoutName;
-//   if (!layoutName) {
-//     return DefaultLayout;
-//   }
-//   return layouts[layoutName];
-// });
+const currentLayout = computed(() => {
+  const layoutName = route.meta.layout as LayoutName;
+  if (!layoutName) {
+    return DefaultLayout;
+  }
+  return layouts[layoutName];
+});
 </script>
 
 <style scoped>
--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,8 +1,8 @@
-import VueI18n from 'vue-i18n'
+import { createI18n } from 'vue-i18n'
 import locales from "@/configs/locales";
 const messages: any = locales.messages;
 
-const i18n = new VueI18n({
+const i18n = createI18n({
   legacy: false,
   locale: "en", // 设置默认语言
   fallbackLocale: "en", // 设置回退语言
